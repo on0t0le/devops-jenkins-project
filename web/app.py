c8eb93f04740ca3beaa55bf3df8de051ccbdd3e0
unknown <bugrovka12@ukr.net>
2019-04-25 22:44:29 +0300

    Change web app

--- web/app.py
+++ web/app.py
@@ -3,6 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, World!"
+    html = "<h3>Hello!</h3>" \
+           "<p>And, again</p>" \
+           "<h2>Welcome to <h2>" \
+           "<h1>The Aperture Sciense computer-aided Enrichment Center</h1><br/>" \
+           "<b>Hostname:</b> {hostname}"
+                      
+    return html.format(hostname=socket.gethostname())
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port=80)
